4126b80473cd15f7b3f73f32c02cc475814cf89c
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-02 11:56:44 +0200

    fixed related activity

--- OIPA/iati/IATI_2_01.py
+++ OIPA/iati/IATI_2_01.py
@@ -1201,6 +1201,7 @@ class Parse(XMLParser):
         related_activity.current_activity = model
         related_activity.type = self.cached_db_call(models.RelatedActivityType,element.attrib.get('code'))
         related_activity.ref = element.attrib.get('ref')
+        related_activity.save()
         return element
 
     '''atributes:
