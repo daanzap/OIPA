c0656bbbd4526c4ed846edd1bb3759b90e6f1052
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-18 11:20:30 +0200

    added test script

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -7,6 +7,7 @@ test_lang = SearchQuerySet().filter(lang='en');
 activity_ids = []
 for activty_id in test_lang:
     activity_ids.append(activty_id.pk)
+
 print 'added ids'
 
 activities = Activity.objects.filter(pk__in=activity_ids)
