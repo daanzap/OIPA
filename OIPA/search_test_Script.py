29fe4358f6f2d08956a1c3e3a886ffdf9b3c17ba
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-18 11:19:46 +0200

    added test script

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -2,12 +2,15 @@ from haystack.query import SearchQuerySet
 from iati.models import Activity
 
 
+print 'before search'
 test_lang = SearchQuerySet().filter(lang='en');
 activity_ids = []
 for activty_id in test_lang:
     activity_ids.append(activty_id.pk)
+print 'added ids'
 
 activities = Activity.objects.filter(pk__in=activity_ids)
+print 'search complete'
 for activity in activities:
     print activity.id
 
