98d220c491e2e6cb2c97ed7396b1d2859cbf1eef
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-21 13:05:13 +0200

    turn of garbage collector

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -1,34 +1,35 @@
 from haystack.query import SearchQuerySet
 from iati.models import Activity
 import time
+import gc
 
 
 
 
 print 'before search'
 start = time.clock() 
-test_lang = SearchQuerySet().filter(lang='en').values_list('pk',flat=True).load_all()
+test_lang = SearchQuerySet().filter(lang='en').values_list('pk',flat=True)
 elapsed = time.clock()
 elapsed = elapsed - start
 print "Time spent in search elastic is: ", elapsed
-print test_lang
-#activity_ids = list(test_lang)
-#print activity_ids
-# start = time.clock()
-# i = 0
-# #print test_lang
-# for activty_id in test_lang:
-# 	#print activty_id
-#    	activity_ids.append(activty_id['pk'])
-#    	i = i+1
+activity_ids = []
+start = time.clock()
+i = 0
+gc.disable()
+#print test_lang
+for activty_id in test_lang:
+	#print activty_id
+   	activity_ids.append(activty_id)
+   	i = i+1
 
-# elapsed = time.clock()
-# elapsed = elapsed - start
-# print "Time spent in adding "+str(i)+" ids is: ", elapsed    
+gc.enable()
+elapsed = time.clock()
+elapsed = elapsed - start
+print "Time spent in adding "+str(i)+" ids is: ", elapsed    
 
 print 'added ids'
 start = time.clock() 
-activities = Activity.objects.filter(pk__in=test_lang)
+activities = Activity.objects.filter(pk__in=list(activity_ids))
 elapsed = time.clock()
 elapsed = elapsed - start
 print "Time spent in (function name) is: ", elapsed
