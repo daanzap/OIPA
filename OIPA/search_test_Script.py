7b9972a8301292d495278137042d5f7176e2e13d
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-21 12:38:58 +0200

    get vlaue list

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -7,24 +7,24 @@ import time
 
 print 'before search'
 start = time.clock() 
-test_lang = SearchQuerySet().filter(lang='en').values('pk');
+test_lang = SearchQuerySet().filter(lang='en').values_list('pk', flat=True)
 elapsed = time.clock()
 elapsed = elapsed - start
 print "Time spent in search elastic is: ", elapsed
-activity_ids = []
-start = time.clock()
-i = 0
-for activty_id in test_lang:
-    activity_ids.append(activty_id['pk'])
-    i = i+1
+# activity_ids = []
+# start = time.clock()
+# i = 0
+# for activty_id in test_lang:
+#     activity_ids.append(activty_id['pk'])
+#     i = i+1
 
-elapsed = time.clock()
-elapsed = elapsed - start
-print "Time spent in adding "+str(i)+" ids is: ", elapsed    
+# elapsed = time.clock()
+# elapsed = elapsed - start
+# print "Time spent in adding "+str(i)+" ids is: ", elapsed    
 
 print 'added ids'
 start = time.clock() 
-activities = Activity.objects.filter(pk__in=list(activity_ids))
+activities = Activity.objects.filter(pk__in=list(test_lang))
 elapsed = time.clock()
 elapsed = elapsed - start
 print "Time spent in (function name) is: ", elapsed
