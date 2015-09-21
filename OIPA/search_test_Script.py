1a65ddde85b906364a0dc77f37c20ecbe5a6e9c2
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-21 12:27:21 +0200

    added more timing to test script

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -6,10 +6,20 @@ import time
 
 
 print 'before search'
+start = time.clock() 
 test_lang = SearchQuerySet().filter(lang='en');
+elapsed = time.clock()
+elapsed = elapsed - start
+print "Time spent in search elastic is: ", elapsed
 activity_ids = []
+start = time.clock()
+i = 0
 for activty_id in test_lang:
     activity_ids.append(activty_id.pk)
+    i = i+1
+elapsed = time.clock()
+elapsed = elapsed - start
+print "Time spent in adding "+str(i)+" ids is: ", elapsed    
 
 print 'added ids'
 start = time.clock() 
