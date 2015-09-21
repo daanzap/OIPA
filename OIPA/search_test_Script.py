bb999ee245b0951d55a5c4f5f702b83b9e9ffd13
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-21 12:23:40 +0200

    added timing to test script

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -1,5 +1,8 @@
 from haystack.query import SearchQuerySet
 from iati.models import Activity
+import time
+
+
 
 
 print 'before search'
@@ -9,7 +12,10 @@ for activty_id in test_lang:
     activity_ids.append(activty_id.pk)
 
 print 'added ids'
-
+start = time.clock() 
 activities = Activity.objects.filter(pk__in=list(activity_ids))
+elapsed = time.clock()
+elapsed = elapsed - start
+print "Time spent in (function name) is: ", elapsed
 print 'search complete'
 print 'done!!!'
