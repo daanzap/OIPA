24ce233a46ffea7782cdfda7267b50ed7a8cfd49
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-21 13:48:21 +0200

    removed itaration over ids

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -12,22 +12,22 @@ test_lang = SearchQuerySet().filter(lang='en').values_list('pk',flat=True)
 elapsed = time.clock()
 elapsed = elapsed - start
 print "Time spent in search elastic is: ", elapsed
-activity_ids = []
-start = time.clock()
-i = 0
-gc.disable()
-#print test_lang
-print type(test_lang)
-for activty_id in test_lang[0:3000000]:
-	#print type(activty_id)
-	#print activty_id
-   	activity_ids.append(activty_id)
-   	i = i+1
+activity_ids = test_lang[:3000000]
+# start = time.clock()
+# i = 0
+# gc.disable()
+# #print test_lang
+# print type(test_lang)
+# for activty_id in test_lang[0:3000000]:
+# 	#print type(activty_id)
+# 	#print activty_id
+#    	activity_ids.append(activty_id)
+#    	i = i+1
 
-gc.enable()
-elapsed = time.clock()
-elapsed = elapsed - start
-print "Time spent in adding "+str(i)+" ids is: ", elapsed    
+# gc.enable()
+# elapsed = time.clock()
+# elapsed = elapsed - start
+# print "Time spent in adding "+str(i)+" ids is: ", elapsed    
 
 print 'added ids'
 start = time.clock() 
