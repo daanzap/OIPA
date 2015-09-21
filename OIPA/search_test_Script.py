6cc709e97cfb3fff18c456d323aed47bec9e917f
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-21 12:52:33 +0200

    skip list creation

--- OIPA/search_test_Script.py
+++ OIPA/search_test_Script.py
@@ -7,26 +7,30 @@ import time
 
 print 'before search'
 start = time.clock() 
-test_lang = SearchQuerySet().filter(lang='en').values('pk');
+test_lang = SearchQuerySet().filter(lang='en').values_list('pk',flat=True)
 elapsed = time.clock()
 elapsed = elapsed - start
 print "Time spent in search elastic is: ", elapsed
-activity_ids = []
-start = time.clock()
-i = 0
-for activty_id in test_lang:
-    activity_ids.append(activty_id['pk'])
-    i = i+1
+activity_ids = list(test_lang)
+#print activity_ids
+# start = time.clock()
+# i = 0
+# #print test_lang
+# for activty_id in test_lang:
+# 	#print activty_id
+#    	activity_ids.append(activty_id['pk'])
+#    	i = i+1
 
-elapsed = time.clock()
-elapsed = elapsed - start
-print "Time spent in adding "+str(i)+" ids is: ", elapsed    
+# elapsed = time.clock()
+# elapsed = elapsed - start
+# print "Time spent in adding "+str(i)+" ids is: ", elapsed    
 
 print 'added ids'
 start = time.clock() 
-activities = Activity.objects.filter(pk__in=list(activity_ids))
+activities = Activity.objects.filter(pk__in=activity_ids)
 elapsed = time.clock()
 elapsed = elapsed - start
 print "Time spent in (function name) is: ", elapsed
 print 'search complete'
 print 'done!!!'
+print len(activities)
