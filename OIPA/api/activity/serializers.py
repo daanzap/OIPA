136d72ffa84a31162d830fbe9741de94cd2abbe9
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2015-09-29 08:09:23 +0200

    temp overhaul of functionality, when the parser's ready this branch can be deleted

--- OIPA/api/activity/serializers.py
+++ OIPA/api/activity/serializers.py
@@ -10,6 +10,7 @@ from api.country.serializers import CountrySerializer
 from api.fields import JSONField
 from api.activity.filters import ActivityFilter, BudgetFilter, RelatedActivityFilter
 
+from django.db.models import Sum
 
 class DocumentLinkSerializer(serializers.ModelSerializer):
     class FileFormatSerializer(serializers.ModelSerializer):
@@ -548,6 +549,19 @@ class ActivitySerializer(DynamicFieldsModelSerializer):
 
     related_activities = RelatedActivitySerializer(many=True, source='current_activity')
 
+    total_child_budgets = serializers.SerializerMethodField()
+
+    def get_total_child_budgets(self, activity):
+        if activity.hierarchy == 1:
+            return iati.models.Activity.objects.filter(
+                    current_activity__related_activity__id=activity,
+                    # current_activity__type__code=2,
+                ).filter(
+                    hierarchy=2,
+                ).aggregate(
+                    total_budget=Sum('budget__value')
+                ).get('total_budget', 0.00)
+
     class Meta:
         model = iati.models.Activity
         fields = (
@@ -577,6 +591,7 @@ class ActivitySerializer(DynamicFieldsModelSerializer):
             'default_aid_type',
             'default_tied_status',
             'budgets',
+            'total_child_budgets',
             'capital_spend',
             'total_budget',
             'xml_source_ref',
